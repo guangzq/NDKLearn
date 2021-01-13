6dfb6360eee24f6fdffd4e5ea12bcddfb0e832d5
zhuqiguang <qiguang.zhu@foxmail.com>
2021-01-13 17:01:16 +0800

    添加静态和动态注册

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,44 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#include "andridloger.h"
+//#include "andridloger.h"
+using namespace std;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_zqg_ndklearn_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    string hello = "Hello from C++";
+
     return env->NewStringUTF(hello.c_str());
 }
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_zqg_ndklearn_MainActivity_addNum(JNIEnv *env, jobject thiz, jint a, jint b) {
+    return a + b;
+}
+
+
+static jint subNum(JNIEnv *env, jobject thiz, jint a, jint b) {
+    return a - b;
+}
+
+static const char *className = "com/zqg/ndklearn/MainActivity";
+static JNINativeMethod methods[] = {{"subNum", "(II)I", (jint *) subNum}};
+
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    LOGD("JNI_INIT");
+    JNIEnv *env;
+    if (vm->GetEnv((void **) (&env), JNI_VERSION_1_4) != JNI_OK) {
+        return JNI_FALSE;
+    }
+    jclass jclass1 = env->FindClass(className);
+    if (env->RegisterNatives(jclass1, methods, sizeof(methods) / sizeof(JNINativeMethod)) > 0) {
+        return JNI_TRUE;
+    }
+    return JNI_VERSION_1_4;
+}
+
--- app/src/main/java/com/zqg/ndklearn/MainActivity.kt
+++ app/src/main/java/com/zqg/ndklearn/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zqg.ndklearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,22 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+        addBtn.setOnClickListener {
+            val left = leftTV.text.toString().toInt()
+            val right = rightTV.text.toString().toInt()
+            showToast(addNum(left, right))
+        }
+
+        subBtn.setOnClickListener {
+            val left = leftTV.text.toString().toInt()
+            val right = rightTV.text.toString().toInt()
+            showToast(subNum(left, right))
+        }
+    }
+
+    fun showToast(str: Int) {
+        Toast.makeText(this, str.toString(), Toast.LENGTH_SHORT).show()
     }
 
     /**
@@ -19,6 +36,8 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun addNum(a: Int, b: Int): Int
+    external fun subNum(a: Int, b: Int): Int
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
